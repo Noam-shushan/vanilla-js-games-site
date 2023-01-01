102e7c0dfd64a0011a958af721bf909208545f86
uditra <yehudaar@g.jct.ac.il>
2023-01-01 14:16:10 +0200

    game 2 fixed

--- js/Games/floppyBird/floppyBird.js
+++ js/Games/floppyBird/floppyBird.js
@@ -1,121 +1,98 @@
 let cvs = document.getElementById("canvas");
-
 let ctx = cvs.getContext("2d");
 
 // load images
-
 let bird = new Image();
-
-let bg = new Image();
-
-let fg = new Image();
-
-let pipeNorth = new Image();
-
-let pipeSouth = new Image();
-
 bird.src = "images/bird.png";
+let bg = new Image();
 bg.src = "images/bg.png";
+let fg = new Image();
 fg.src = "images/fg.png";
+let pipeNorth = new Image();
 pipeNorth.src = "images/pipeNorth.png";
+let pipeSouth = new Image();
 pipeSouth.src = "images/pipeSouth.png";
 
-// some letiables
-
-let gap = 110;
-
-let constant;
-
-let bX = 10;
-
-let bY = 150;
-
-let gravity = 1.8;
-let score = 0;
-
 // audio files
-
 let fly = new Audio();
-
-let scor = new Audio();
-
 fly.src = "sounds/fly.mp3";
+let scor = new Audio();
 scor.src = "sounds/score.mp3";
 
-// on key down
-document.addEventListener("keydown", moveUp);
-function moveUp() {
-    bY -= 30;
-    if (bY < 4)
-        bY = 4
-    fly.play();
-}
+function play()
+{
+    // some letiables
+    let gap = 110;
+    let constant;
+    let bX = 10;
+    let bY = 150;
+    let gravity = 1.8;
+    let score = 0;
 
-// pipe coordinates
-
-let pipe = [];
-
-pipe[0] = {
-    x: cvs.width,
-    y: 0
-};
-
-// Setting initial game state to start
-let game_state = 'Start';
-
-// draw images
-
-function draw() {
-
-    // Detect if game has ended
-    if (game_state != 'Start') return;
-
-    ctx.drawImage(bg, 0, 0);
-    for (let i = 0; i < pipe.length; i++) {
-        constant = pipeNorth.height + gap;
-
-        ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
-
-        ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
-
-        pipe[i].x--;
-
-        if (pipe[i].x == 125) {
-
-            pipe.push({
-                x: cvs.width,
-                y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
-            });
+    // on key down
+    document.addEventListener("keydown",(e) => {
+        // Start the game if enter key is pressed
+        if (e.key == 'ArrowUp' && game_state != 'End') 
+        {
+            bY -= 30;
+            if (bY < 4)
+                bY = 4
+            fly.play();
         }
-        // detect collision
+    });
 
-        if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y + constant) || bY + bird.height >= cvs.height - fg.height) {
-            game_state = 'End';
-            ////location.reload(); // reload the page
-            let s = document.createElement('script');
-            s.src = "./js/Games/floppyBird/floppyBird.js";
-            document.body.appendChild(s);
+    // pipe coordinates
+    let pipe = [];
+    pipe[0] = {
+        x: cvs.width,
+        y: 0
+    };
+
+    // Setting initial game state to start
+    let game_state = 'Play';
+
+    // draw images
+    function draw() {
+        // Detect if game has ended
+        if (game_state == 'End') return;
+        ctx.drawImage(bg, 0, 0);
+        for (let i = 0; i < pipe.length; i++) {
+            constant = pipeNorth.height + gap;
+            ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+            ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
+            pipe[i].x--;
+            if (pipe[i].x == 110) {
+                pipe.push({
+                    x: cvs.width,
+                    y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
+                });
+            }
+            // detect collision
+            if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y + constant) || bY + bird.height >= cvs.height - fg.height) {
+                game_state = 'End';
+                ////location.reload(); // reload the page
+                document.addEventListener("keydown",(e) => {
+                    // Start the game if enter key is pressed
+                    if (e.key == 'Enter' && game_state != 'Play') 
+                    {
+                        play();
+                    }
+                });
+            }
+            if (pipe[i].x == 5) {
+                score++;
+                scor.play();
+                gap--;
+            }
         }
-        if (pipe[i].x == 5) {
-            score++;
-            scor.play();
-            gap--;
-        }
+        ctx.drawImage(fg, 0, cvs.height - fg.height);
+        ctx.drawImage(bird, bX, bY);
+        bY += gravity;
+        ctx.fillStyle = "#000";
+        ctx.font = "20px Verdana";
+        ctx.fillText("Score : " + score, 10, cvs.height - 20);
+        requestAnimationFrame(draw);
     }
-    ctx.drawImage(fg, 0, cvs.height - fg.height);
-
-    ctx.drawImage(bird, bX, bY);
-
-    bY += gravity;
-    ctx.fillStyle = "#000";
-    ctx.font = "20px Verdana";
-
-    ctx.fillText("Score : " + score, 10, cvs.height - 20);
-
-    requestAnimationFrame(draw);
+    draw();
 }
-
-draw();
-
-
-
+play()
