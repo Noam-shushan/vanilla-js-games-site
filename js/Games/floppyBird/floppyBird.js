d6cc6dcc1358780024437ce747f56004a7628324
Noam shushan <noam8shu@gmail.com>
2023-01-01 13:12:41 +0200

    potential fix of floppy

--- js/Games/floppyBird/floppyBird.js
+++ js/Games/floppyBird/floppyBird.js
@@ -1,18 +1,18 @@
-var cvs = document.getElementById("canvas");
+let cvs = document.getElementById("canvas");
 
-var ctx = cvs.getContext("2d");
+let ctx = cvs.getContext("2d");
 
 // load images
 
-var bird = new Image();
+let bird = new Image();
 
-var bg = new Image();
+let bg = new Image();
 
-var fg = new Image();
+let fg = new Image();
 
-var pipeNorth = new Image();
+let pipeNorth = new Image();
 
-var pipeSouth = new Image();
+let pipeSouth = new Image();
 
 bird.src = "images/bird.png";
 
@@ -23,25 +23,25 @@ pipeNorth.src = "images/pipeNorth.png";
 
 pipeSouth.src = "images/pipeSouth.png";
 
-// some variables
+// some letiables
 
-var gap = 110;
+let gap = 110;
 
-var constant;
+let constant;
 
-var bX = 10;
+let bX = 10;
 
-var bY = 150;
+let bY = 150;
 
-var gravity = 1.8;
+let gravity = 1.8;
 
-var score = 0;
+let score = 0;
 
 // audio files
 
-var fly = new Audio();
+let fly = new Audio();
 
-var scor = new Audio();
+let scor = new Audio();
 
 fly.src = "sounds/fly.mp3";
 
@@ -49,60 +49,63 @@ scor.src = "sounds/score.mp3";
 
 // on key down
 
-document.addEventListener("keydown",moveUp);
+document.addEventListener("keydown", moveUp);
 
-function moveUp(){
+function moveUp() {
     bY -= 30;
-    if(bY<4)
+    if (bY < 4)
         bY = 4
     fly.play();
 }
 
 // pipe coordinates
 
-var pipe = [];
+let pipe = [];
 
 pipe[0] = {
-    x : cvs.width,
-    y : 0
+    x: cvs.width,
+    y: 0
 };
 
 // draw images
 
-function draw(){
-    ctx.drawImage(bg,0,0);
+function draw() {
+    ctx.drawImage(bg, 0, 0);
 
-    for(var i = 0; i < pipe.length; i++){
-        constant = pipeNorth.height+gap;
+    for (let i = 0; i < pipe.length; i++) {
+        constant = pipeNorth.height + gap;
 
-        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
+        ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
 
-        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
+        ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
 
         pipe[i].x--;
 
-        if( pipe[i].x == 125 ){
+        if (pipe[i].x == 125) {
 
             pipe.push({
-                x : cvs.width,
-                y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
+                x: cvs.width,
+                y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
             });
         }
 
         // detect collision
 
-        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
-            location.reload(); // reload the page
+        if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y + constant) || bY + bird.height >= cvs.height - fg.height) {
+            //location.reload(); // reload the page
+            let s = document.createElement('script');
+            s.src = "./js/Games/floppyBird/floppyBird.js";
+            document.body.appendChild(s);
         }
-        if(pipe[i].x == 5){
+        if (pipe[i].x == 5) {
             score++;
             scor.play();
             gap--;
         }
     }
-    ctx.drawImage(fg,0,cvs.height - fg.height);
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
 
-    ctx.drawImage(bird,bX,bY);
+    ctx.drawImage(bird, bX, bY);
 
     bY += gravity;
 
@@ -110,8 +113,12 @@ function draw(){
 
     ctx.font = "20px Verdana";
 
-    ctx.fillText("Score : "+score,10,cvs.height-20);
+    ctx.fillText("Score : " + score, 10, cvs.height - 20);
 
     requestAnimationFrame(draw);
 }
+
 draw();
+
+
+
