a6da4f19682ebdd1d98c4ebdbec03c3d1199cced
uditra <yehudaar@g.jct.ac.il>
2023-01-02 18:41:30 +0200

    turned back

--- js/Games/floppyBird/floppyBird.js
+++ js/Games/floppyBird/floppyBird.js
@@ -19,13 +19,7 @@ fly.src = "sounds/fly.mp3";
 let scor = new Audio();
 scor.src = "sounds/score.mp3";
 
-function updateUser(user,setCurrentUser,score,date)
-{
-    user.scores.push({game:"floppyBird",score: score,time:date})
-    setCurrentUser(user)
-}
-
-function play(user,setCurrentUser) {
+function play() {
 
     // some letiables
     let gap = 110;
@@ -75,7 +69,6 @@ function play(user,setCurrentUser) {
             // detect collision
             if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y + constant) || bY + bird.height >= cvs.height - fg.height) {
                 game_state = 'End';
-                updateUser(user,setCurrentUser,score,Date())
                 ////location.reload(); // reload the page
                 document.addEventListener("keydown", (e) => {
                     // Start the game if enter key is pressed
--- js/views/floppyBird-game-view.js
+++ js/views/floppyBird-game-view.js
@@ -1,6 +1,4 @@
 import { play } from "../Games/floppyBird/floppyBird.js";
 import { getCurrentUser , setCurrentUser} from "../storageHandler.js";
 
-let user = getCurrentUser()
-console.log("hi")
-play(user,setCurrentUser);
+play();
